864feb4fa1845b421e65bebfb2877b71f40c7689
Erkin Ovlyagulyyev <mail@erkin.me>
2025-05-07 01:35:07 +0300

    refactor: simplify request payload by removing redundant string prefixes

--- js/openrouter.js
+++ js/openrouter.js
@@ -89,9 +89,9 @@ class AIAssistant {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            prompt: 'User: ' + prompt,
-            context: 'Context: ' + context,
-            chatHistory: 'Chat History: ' + chatHistory
+            prompt,
+            context,
+            chatHistory
           })
         });
         
